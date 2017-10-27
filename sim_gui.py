c90f729f94f832e848cf77d17499d72192cfe2e7
Udaya Kumar <udayakumar97@gmail.com>
2017-10-27 10:47:07 +0530

    Added more features

--- sim_gui.py
+++ sim_gui.py
@@ -1,5 +1,11 @@
 from appJar import gui
 import scheduler
+import random
+import string
+
+def id_generator(size=6, chars=string.ascii_lowercase): #generate the random string of n size and from given sentence
+    return ''.join(random.choice(chars) for _ in range(size))
+
 def num_tasks_press(btn):
     num_tasks=app.getEntry('num_tasks_entry')
     print(num_tasks)
@@ -16,25 +22,34 @@ def task_details_press(btn):
         task_details.append(dict(name='Task '+str(i),entry=entry,burst=burst,memory=memory))
     print(task_details)
     scheduler.simple_scheduler(task_details)
-    app.addImage("Simple Scheduler", "simple.png")
-    app.zoomImage("Simple Scheduler", -2)
+    try:
+        app.destroySubWindow("gannt-chart")
+    except Exception as ex:
+        print(ex)
+    app.startSubWindow("gannt-chart")
+    image_window_name=id_generator()
+    app.addImage(image_window_name, "simple.png")
+    app.stopSubWindow()
+    app.showSubWindow("gannt-chart")
+    
+    
 
 
 def generate_task_details_column(num_tasks):
-    app.addLabel("total_memory_label","Enter total memory of system")
-    app.addNumericEntry('total_memory_entry')
+    app.addLabel("total_memory_label","Enter total memory of system",1,0)
+    app.addNumericEntry('total_memory_entry',1,1)
     for i in range(0,num_tasks):
-        app.addLabel("task"+str(i)+"_label","TASK"+str(i))
-        app.addLabel("entry"+str(i)+"_label","Enter entry time")
-        app.addNumericEntry('entry_time'+str(i)+"_entry")
-        app.addLabel("burst_time"+str(i)+"_label","Enter burst time")
-        app.addNumericEntry('burst_time'+str(i)+"_entry")
-        app.addLabel("memory"+str(i)+"_label","Enter memory")
-        app.addNumericEntry('memory'+str(i)+"_entry")
+        app.addLabel("task"+str(i)+"_label","TASK"+str(i),i+2,0)
+        app.addLabel("entry"+str(i)+"_label","Enter entry time",i+2,1)
+        app.addNumericEntry('entry_time'+str(i)+"_entry",i+2,2)
+        app.addLabel("burst_time"+str(i)+"_label","Enter burst time",i+2,3)
+        app.addNumericEntry('burst_time'+str(i)+"_entry",i+2,4)
+        app.addLabel("memory"+str(i)+"_label","Enter memory",i+2,5)
+        app.addNumericEntry('memory'+str(i)+"_entry",i+2,6)
     app.addNamedButton('Enter','task_details_btn',task_details_press)
 
 app=gui()
-app.addLabel("num_tasks_label","Number of tasks")
-app.addNumericEntry('num_tasks_entry')
-app.addNamedButton('Enter','num_task_btn',num_tasks_press)
+app.addLabel("num_tasks_label","Number of tasks",0,0)
+app.addNumericEntry('num_tasks_entry',0,1)
+app.addNamedButton('Enter','num_task_btn',num_tasks_press,0,2)
 app.go()
